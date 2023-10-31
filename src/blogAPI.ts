a981984c42faaa7ce70d054ea24a50783d1d3d70
nakauchannel <nakau.tech@gmail.com>
2023-11-01 06:38:18 +0900

    ✨ blogAPI作成

---
+++ src/blogAPI.ts
@@ -0,0 +1,34 @@
+import { Article } from "./types";
+import { notFound } from "next/navigation";
+
+export const getAllArticles = async (): Promise<Article[]> => {
+  const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" }); //SSR
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  const articles = await res.json(); //シリアライズ　データをJSON形式に
+  return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); //SSR
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  const article = await res.json(); //シリアライズ　データをJSON形式に
+  return article;
+};
